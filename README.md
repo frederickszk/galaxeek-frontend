1eba78aa9bf1786bad508144f5edc59b76619fcf
frederickszk <45565991+frederickszk@users.noreply.github.com>
2022-01-08 23:24:14 +0800

    Adding and optimizing the map

--- README.md
+++ README.md
@@ -15,8 +15,35 @@ $ yarn start
 ```
 
 
-# TODOs
+# Notes
 
-## 12/25
-- Check the logic for enforcing Login and autoLogin (in `currentUser.mock.ts`).
-- Ensure where is the navigator links logic (e.g. redirect to `\list`)
+## 框架相关
+- 自动登录逻辑 (`currentUser.mock.ts`).
+
+原模板并未实现自动登录相关，只是简单的保存了一个全局变量`access`，以此判定是否需要登录。
+
+- 菜单栏逻辑
+
+左边的navigator在antdesign-prolayout框架下由`route.ts`中给页面设定`name`和`icon`可自动生成。`icon`选择可参考[antd-icon](https://ant.design/components/icon-cn/).
+> 注意命名直接使用实体部分小写，比如皇冠标志类名为`CrownOutlined`，则写`crown`.
+
+## 地图接入相关
+- 使用的框架：[Leaflet](https://leafletjs.com/download.html)
+- 接入方式
+
+在线引入不好用，因为全局的 `index.html`不方便直接控制。所以最好还是直接利用`npm install leaflet`安装。
+
+安装完毕后在页面文件中引入：
+```Typescript
+import L from 'leaflet';
+import "leaflet/dist/leaflet.css"
+```
+之后可按照教程进行其它操作。注意第二行的样式文件很重要，不引入可能导致地图加载不正常，会变成一块一块的小方格。
+
+- 动态调整地图高度
+
+页面挂载及变化时用`document.getElementById()`直接调整`<div>`的高度。注意为了避免`null`报错可使用下述断言操作符`!`：
+```Typescript
+document.getElementById('test')!.xxx.xxx
+```
+> 参考教程：[Blog1](https://blog.csdn.net/weixin_30645617/article/details/96842586), [Blog2](https://blog.csdn.net/m0_37697423/article/details/106100922)
--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -4,80 +4,100 @@ import { Spin } from 'antd';
 import styles from './index.less';
 import L from 'leaflet';
 import React from 'react';
+import 'leaflet/dist/leaflet.css';
 
-// const Map: React.Component = () => {
-//   const [loading, setLoading] = useState<boolean>(true);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setLoading(false);
-//     }, 3000);
-//   }, []);
+export default () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
-//   componentDidMount()
+  const [mapInit, setMapInit] = useState<boolean>(false);
 
-//   const setupMap = () => {
+  useEffect(() => {
+    if (!mapInit) {
+      var map = L.map('map').setView([51.505, -0.09], 13);
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken:
+            'pk.eyJ1IjoiZnJlZGVyaWNrc3VuIiwiYSI6ImNreTU5bHFzczBmZTAycG9hZTV1YmtlcTUifQ.xQPSws8NDy78VJQ4TwRsZQ',
+        },
+      ).addTo(map);
+
+      // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      // }).addTo(map);
+
+      var h = document.body.clientHeight;
+      var mapHeight = Math.floor(h * 0.8);
+
+      document.getElementById('map')!.style.height = mapHeight + 'px';
+      map.invalidateSize(true);
+
+      setMapInit(true);
+    }
+  }, [mapInit]);
+
+  return (
+    <PageContainer
+      content="这是一个新页面，从这里进行开发！"
+      className={styles.main}
+    >
+      {/* <div style={{ paddingTop: 100, textAlign: 'center' }}>
+        <Spin spinning={loading} size="large" />
+      </div> */}
+
+      <div id="map" className={styles.map}></div>
+    </PageContainer>
+  );
+};
+
+// class Map extends React.Component {
+//   // const [loading, setLoading] = useState<boolean>(true);
+//   // useEffect(() => {
+//   //   setTimeout(() => {
+//   //     setLoading(false);
+//   //   }, 3000);
+//   // }, []);
+
+//   componentDidMount() {
 //     var map = L.map('map').setView([51.505, -0.09], 13);
-//     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'your.mapbox.access.token'
+//     // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     // maxZoom: 18,
+//     // id: 'mapbox/streets-v11',
+//     // tileSize: 512,
+//     // zoomOffset: -1,
+//     // accessToken: 'your.mapbox.access.token'
+//     // }).addTo(map);
+//     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//       attribution:
+//         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+//       maxZoom: 18,
 //     }).addTo(map);
 //   }
 
-//   return (
-//     <PageContainer content="这是一个新页面，从这里进行开发！" className={styles.main}>
-
-//       <div style={{ paddingTop: 100, textAlign: 'center' }}>
+//   render() {
+//     return (
+//       <PageContainer
+//         content="这是一个新页面，从这里进行开发！"
+//         className={styles.main}
+//       >
+//         {/* <div style={{ paddingTop: 100, textAlign: 'center' }}>
 //         <Spin spinning={loading} size="large" />
-//       </div>
-
-//       <div id="map"></div>
-//       {setupMap()}
-
-//     </PageContainer>
-//   );
+//         </div> */}
+//         <div id="map" className={styles.map}></div>
+//       </PageContainer>
+//     );
+//   }
 // }
-
-class Map extends React.Component {
-  // const [loading, setLoading] = useState<boolean>(true);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 3000);
-  // }, []);
-
-  componentDidMount() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    // maxZoom: 18,
-    // id: 'mapbox/streets-v11',
-    // tileSize: 512,
-    // zoomOffset: -1,
-    // accessToken: 'your.mapbox.access.token'
-    // }).addTo(map);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
-  }
-
-  render() {
-    return (
-      <PageContainer
-        content="这是一个新页面，从这里进行开发！"
-        className={styles.main}
-      >
-        {/* <div style={{ paddingTop: 100, textAlign: 'center' }}>
-        <Spin spinning={loading} size="large" />
-        </div> */}
-        <div id="map" className={styles.map}></div>
-      </PageContainer>
-    );
-  }
-}
-export default Map;
+// export default Map;
